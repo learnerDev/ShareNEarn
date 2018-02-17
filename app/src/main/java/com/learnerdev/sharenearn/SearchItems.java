bc3ebfe22add719693896d1ef1ca08a80dc7cc56
leanerDev <30748943+learnerDev@users.noreply.github.com>
2018-02-17 15:26:47 +0530

    test-commit-no-much-change

--- app/src/main/java/com/learnerdev/sharenearn/SearchItems.java
+++ app/src/main/java/com/learnerdev/sharenearn/SearchItems.java
@@ -1,13 +1,21 @@
 package com.learnerdev.sharenearn;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SearchItems extends AppCompatActivity {
+    static final String TAG = "ShareNEarn-SearchItems";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_items);
+        initUI();
     }
+
+    private void initUI() {
+
+    }
+
 }
+
